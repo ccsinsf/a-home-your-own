8e8668ecb02347e4f6fb177a4f8472f681f7f90f
ccsinsf <catherine.scott3@gmail.com>
2019-02-12 14:50:21 -0800

    updated seed.py, about to build database

--- server.py
+++ server.py
@@ -1,31 +1,45 @@
-from flask import Flask, redirect, request, render_template, session
-from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 
+from flask import (Flask, render_template, redirect, request, flash, session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import User, connect_to_db, db
+
+
 app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails
+# silently. This is horrible. Fix this so that, instead, it raises an
+# error.
 app.jinja_env.undefined = StrictUndefined
-app.jinja_env.auto_reload = True
-app.config.update(DEBUG_TB_INTERCEPT_REDIRECTS = False)
-# used this website to learn more about Flask config http://flask.pocoo.org/docs/1.0/config/
 
 
-@app.route("/")
-def show_homepage_and_form():
-    """Show the user our landing page and show user form"""
-    
+@app.route('/')
+def index():
+    """Homepage."""
     return render_template("homepage.html")
 
+@app.route("/users")
+def user_list():
+    """Show list of users."""
 
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
 
-    
-# ------- #
 
 if __name__ == "__main__":
-# We have to set debug=True here, since it has to be True at the
-# point that we invoke the DebugToolbarExtension
-app.debug = True
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
 
-# Use the DebugToolbar
-DebugToolbarExtension(app)
+    connect_to_db(app)
 
-app.run(host="0.0.0.0")
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
